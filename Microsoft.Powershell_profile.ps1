ce81a24680dc12615602ae2d83205f4cced369ef
Frank Fu <frankfu1122@gmail.com>
2018-01-22 19:33:32 +1100

    Update Microsoft.Powershell_profile.ps1

--- Microsoft.Powershell_profile.ps1
+++ Microsoft.Powershell_profile.ps1
@@ -1,4 +1,5 @@
 Import-Module -Name posh-git
+Import-Module -Name posh-docker
 
 function Test-Administrator {
     $user = [Security.Principal.WindowsIdentity]::GetCurrent();
@@ -36,6 +37,6 @@ function prompt {
 Set-Alias ssh-agent "$env:ProgramFiles\git\usr\bin\ssh-agent.exe"
 Set-Alias ssh-add "$env:ProgramFiles\git\usr\bin\ssh-add.exe"
 
-Set-Alias sublime "C:\Program Files\Sublime Text 3\sublime_text.exe"
+Set-Alias sublime3 "C:\Program Files\Sublime Text 3\sublime_text.exe"
 
 Start-SshAgent
