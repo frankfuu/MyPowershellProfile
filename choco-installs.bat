eae4d70bc275655fa72cea1ca7e8fb66cb5fe07c
Frank Fu <frankfu1122@gmail.com>
2018-03-09 11:17:28 +1100

    Create choco-installs.bat

---
+++ choco-installs.bat
@@ -0,0 +1,47 @@
+
+choco feature enable -n=allowGlobalConfirmation
+
+# Crucial
+choco install 7zip
+choco install google-chrome
+choco install firefox
+choco install cmder
+choco install ccleaner
+
+# Editors
+choco install sublimetext3
+choco install notepadplusplus
+choco install visualstudiocode
+
+# Dev crucial
+choco install git
+choco install nodejs
+choco install ruby
+choco install python3
+choco install pip
+choco install beyondcompare
+choco install webpi
+choco install nuget.commandline
+
+# Dev optional
+choco install gifcam
+choco install fiddler4
+choco install mongodb
+choco install conemu
+choco install filezilla
+choco install linqpad
+
+# Utils
+choco install adobereader
+choco install teamviewer
+choco install spotify
+choco install youtube-dl
+choco install ffmpeg
+
+# Utils optional
+choco install freecommander
+choco install rdcman
+choco install aria2
+choco install axel
+
+pause
