04622dc35c9a92eafbac52897cb5041a3dcfcc34
Frank Fu <frankfu1122@gmail.com>
2018-08-02 22:39:22 +1000

    Update Microsoft.Powershell_profile.ps1

--- Microsoft.Powershell_profile.ps1
+++ Microsoft.Powershell_profile.ps1
@@ -36,8 +36,6 @@ function prompt {
 }
 
 # Create aliases
-Set-Alias ssh-agent "$env:ProgramFiles\git\usr\bin\ssh-agent.exe"
-Set-Alias ssh-add "$env:ProgramFiles\git\usr\bin\ssh-add.exe"
 Set-Alias sublime "C:\Program Files\Sublime Text 3\sublime_text.exe"
 Set-Alias code "C:\Program Files\Microsoft VS Code\Code.exe"
 
