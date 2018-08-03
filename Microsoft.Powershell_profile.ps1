0da3004bf97dcfd221b4a63c4d09cb8e17b0ccfb
Frank Fu <frankfu1122@gmail.com>
2018-08-03 11:39:20 +1000

    Update Microsoft.Powershell_profile.ps1

--- Microsoft.Powershell_profile.ps1
+++ Microsoft.Powershell_profile.ps1
@@ -1,40 +1,20 @@
-# https://github.com/dahlbyk/posh-git/issues/583 - Windows 10 version 1803 broke my ssh-agent
-[System.Environment]::SetEnvironmentVariable("SSH_AUTH_SOCK", $null)
-[System.Environment]::SetEnvironmentVariable("SSH_AGENT_PID", $null)
-git config --global core.sshCommand C:/Windows/System32/OpenSSH/ssh.exe
-
-# Install desired modules
-if (Get-Module -ListAvailable -Name DockerCompletion) {    
-    Import-Module DockerCompletion
-} else {
-    Install-Module DockerCompletion -Force
-    Import-Module DockerCompletion
-}
-
-if (Get-Module -ListAvailable -Name GetSTFolderSize) {    
-    Import-Module GetSTFolderSize
-} else {
-    Install-Module GetSTFolderSize -Force
-    Import-Module GetSTFolderSize
-}
-
-function Test-Administrator {
+Function Test-Administrator {
     $user = [Security.Principal.WindowsIdentity]::GetCurrent();
     (New-Object Security.Principal.WindowsPrincipal $user).IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
 }
 
-function prompt {
+Function prompt {
     # https://github.com/dahlbyk/posh-git/wiki/Customizing-Your-PowerShell-Prompt
     $origLastExitCode = $LastExitCode
     Write-VcsStatus
 
     if (Test-Administrator) {  # if elevated
-        Write-Host "[Administrator] " -NoNewline -ForegroundColor DarkGreen
+        Write-Host "[Administrator] " -NoNewline -ForegroundColor Green
     }
 
     Write-Host "$env:USERNAME@" -NoNewline -ForegroundColor Yellow
     Write-Host "$env:COMPUTERNAME" -NoNewline -ForegroundColor Magenta
-    Write-Host " : " -NoNewline -ForegroundColor DarkGray
+    Write-Host " - " -NoNewline -ForegroundColor DarkGray
 
     $curPath = $ExecutionContext.SessionState.Path.CurrentLocation.Path
     if ($curPath.ToLower().StartsWith($Home.ToLower()))
@@ -42,14 +22,53 @@ function prompt {
         $curPath = "~" + $curPath.SubString($Home.Length)
     }
 
-    Write-Host $curPath -NoNewline -ForegroundColor DarkGray
-    Write-Host " : " -NoNewline -ForegroundColor DarkGray
-    Write-Host $(get-date) -NoNewline -ForegroundColor Green    
+    Write-Host $curPath -NoNewline -ForegroundColor Cyan
+    Write-Host " @ " -NoNewline -ForegroundColor DarkGreen
+    Write-Host $(get-date) -NoNewline -ForegroundColor DarkGreen    
     $LastExitCode = $origLastExitCode
-    "`n$('> ' * ($nestedPromptLevel + 1)) "
+    "`n$('>' * ($nestedPromptLevel + 1)) "
 }
 
-# Create aliases
-Set-Alias sublime "C:\Program Files\Sublime Text 3\sublime_text.exe"
-Set-Alias code "C:\Program Files\Microsoft VS Code\Code.exe"
+Function Configure-GitUseOpenSSHWorkaround
+{
+    # https://github.com/dahlbyk/posh-git/issues/583 - Windows 10 version 1803 broke my ssh-agent
+    [System.Environment]::SetEnvironmentVariable("SSH_AUTH_SOCK", $null)
+    [System.Environment]::SetEnvironmentVariable("SSH_AGENT_PID", $null)
+    git config --global core.sshCommand C:/Windows/System32/OpenSSH/ssh.exe
 
+}
+
+Function Add-PersonalModules 
+{
+    if (Get-Module -ListAvailable -Name DockerCompletion) {    
+        Import-Module DockerCompletion
+    } else {
+        Write-Host "DockerCompletion required but not found. Installing now .."
+        Install-Module DockerCompletion -Force
+        Import-Module DockerCompletion
+    }
+    
+    if (Get-Module -ListAvailable -Name GetSTFolderSize) {    
+        Import-Module GetSTFolderSize
+    } else {
+        Write-Host "GetSTFolderSize required but not found. Installing now .."
+        Install-Module GetSTFolderSize -Force
+        Import-Module GetSTFolderSize
+    }
+}
+
+Function Add-PersonalAliases
+{
+    Set-Alias sublime "C:\Program Files\Sublime Text 3\sublime_text.exe"
+    Set-Alias code "C:\Program Files\Microsoft VS Code\Code.exe"
+}
+
+
+
+# Begin script
+
+Add-PersonalModules
+Add-PersonalAliases
+Configure-GitUseOpenSSHWorkaround
+
+Write-Host "Loaded PS Profile."
