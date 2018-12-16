b486304f54189d8b36dfb6f0f3a80ba12714360c
Frank Fu <frankfu1122@gmail.com>
2018-12-16 15:51:24 +1100

    Update bootstrap-windows.ps1
    Force create Symlink

--- bootstrap-windows.ps1
+++ bootstrap-windows.ps1
@@ -1,3 +1,3 @@
 # Add a new Powershell Profile symlink to this repo and reload.
-New-Item -Type SymbolicLink -Path $PROFILE -Value .\windows\Microsoft.Powershell_profile.ps1
+New-Item -Type SymbolicLink -Path $PROFILE -Value .\windows\Microsoft.Powershell_profile.ps1 -Force
 . $PROFILE
