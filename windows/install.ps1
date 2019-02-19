97cd20ad81e6444038e9c265244b56cb30d4086c
Frank Fu <frankfu1122@gmail.com>
2019-02-19 18:30:28 +1100

    Add scheduled task to periodically pull dotfiles on windows

--- windows/install.ps1
+++ windows/install.ps1
@@ -3,5 +3,11 @@ New-Item -Type SymbolicLink -Path $PROFILE -Value .\Microsoft.Powershell_profile
 New-Item -Type SymbolicLink -Path $env:USERPROFILE\.gitconfig -Value .\.gitconfig -Force
 New-Item -Type SymbolicLink -Path $env:USERPROFILE\.gitignore_global -Value .\.gitignore_global -Force
 
+# Create sheduled task to periodically pull
+$startTrigger =  New-ScheduledTaskTrigger -Daily -At 6pm
+$sAction = New-ScheduledTaskAction -Execute "git.exe" -Argument '-C "c:\projects\dotfiles" pull --verbose'
+$taskName = "pull dotfiles"
+Register-ScheduledTask -Action $sAction -TaskName $taskName -TaskPath $env:USERNAME -Trigger $startTrigger
+
 # Reload Powershell profile
 . $PROFILE
--- windows/uninstall.ps1
+++ windows/uninstall.ps1
@@ -2,3 +2,7 @@
 Remove-Item $PROFILE
 Remove-Item $env:USERPROFILE\.gitconfig
 Remove-Item $env:USERPROFILE\.gitignore_global
+
+# Unregister periodic pull
+$taskName = "pull dotfiles"
+Unregister-ScheduledTask -TaskName $taskName -Confirm:$false
