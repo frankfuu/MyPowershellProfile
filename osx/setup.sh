326f69fb6fcfc2d0ab036bba2bfbebbc01b23bac
Frank Fu <frankfu1122@gmail.com>
2019-08-13 18:12:52 +1000

    Adding some initial setup for macOS and also iterm2 theme

---
+++ osx/setup.sh
@@ -0,0 +1,13 @@
+
+# install brew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# install dev essentials
+brew cask install visual-studio-code
+brew cask install virtualbox 
+brew cask install vagrant
+brew cask install vagrant-manager
+brew cask install iterm2
+
+# add basic vagrant boxes
+vagrant box add ubuntu/xenial64
