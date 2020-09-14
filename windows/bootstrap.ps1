e6730eefc7ff476d6e0f7263ceeffbe956f48c20
Frank Fu <frankfu1122@gmail.com>
2020-09-14 07:04:46 -0700

    update bootstrap script

--- windows/bootstrap.ps1
+++ windows/bootstrap.ps1
@@ -1,4 +1,6 @@
-Get-Service ssh-agent | Set-Service -StartupType Automatic
-Get-Service ssh-agent | Start-Service
-git clone https://github.com/frankfuu/dotfiles.git
+Get-Service ssh-agent | Set-Service -StartupType Automatic;
+Get-Service ssh-agent | Start-Service;
+git clone https://github.com/frankfuu/dotfiles.git;
+cd dotfiles;
 git remote set-url origin git@github.com:frankfuu/dotfiles.git
+.\install.ps1
